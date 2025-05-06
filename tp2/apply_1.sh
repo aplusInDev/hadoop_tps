d07edd86aa489732afe1ddd5db6922a29fc5b6ac
aplusInDev <laabid.abdessamadplus@gmail.com>
2025-05-06 15:48:08 +0100

    udating tp2 hadoop jobs application

--- tp2/apply_1.sh
+++ tp2/apply_1.sh
@@ -1,13 +1,15 @@
 hdfs dfs -mkdir -p /data/tp2/input_1
-hdfs dfs -copyFromLocal ~/tp2/meteosample.txt /data/tp2/input_1/
+hdfs dfs -copyFromLocal ~/hadoop_tps/tp2/meteosample.txt /data/tp2/input_1/
 
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
-    -file ~/tp2/mapper_1.py -mapper "python3 mapper_1.py" \
-    -file ~/tp2/reducer_1.py -reducer "python3 reducer_1.py" \
-    -input /data/tp2/input_1/* \
-    -output /output_1
+  -files ~/hadoop_tps/tp2/mapper_1.py,~/hadoop_tps/tp2/reducer_1.py \
+  -mapper "python3 mapper_1.py" \
+  -reducer "python3 reducer_1.py" \
+  -input /data/tp2/input_1/* \
+  -output /output_1
 
-hdfs dfs -cat /output_1/part-00000
+echo "Results: ------------------";
+hdfs dfs -cat /output_1/part-*
 
 hdfs dfs -rm -r /output_1
 hdfs dfs -rm -r /data/tp2/
--- tp2/apply_2.sh
+++ tp2/apply_2.sh
@@ -1,13 +1,17 @@
+THRESHOLD=0
+
 hdfs dfs -mkdir -p /data/tp2/input_2
-hdfs dfs -copyFromLocal ~/tp2/meteosample.txt /data/tp2/input_2/
+hdfs dfs -copyFromLocal ~/hadoop_tps/tp2/meteosample.txt /data/tp2/input_2/
 
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
-    -file ~/tp2/mapper_2.py -mapper "python3 mapper_2.py 0" \
-    -file ~/tp2/reducer_2.py -reducer "python3 reducer_2.py" \
+    -files ~/hadoop_tps/tp2/mapper_2.py,~/hadoop_tps/tp2/reducer_2.py \
+    -mapper "python3 mapper_2.py $THRESHOLD" \
+    -reducer "python3 reducer_2.py" \
     -input /data/tp2/input_2/* \
     -output /output_2
 
-hdfs dfs -cat /output_2/part-00000
+echo "Results for Months with Temperature > $THRESHOLD: ------------------";
+hdfs dfs -cat /output_2/part-*
 
 hdfs dfs -rm -r /output_2
 hdfs dfs -rm -r /data/tp2/
