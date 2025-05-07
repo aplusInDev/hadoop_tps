bf2430aba709477e1e172d57db0d6574af2f4d96
aplusInDev <laabid.abdessamadplus@gmail.com>
2025-05-07 23:08:23 +0100

    removing unecessary comments

--- tp2/reducer_2.py
+++ tp2/reducer_2.py
@@ -2,23 +2,21 @@
 import sys
 
 def reducer():
-    current_month = None
-    unique_months = set()  # Track unique months
+    unique_months = set()
 
     for line in sys.stdin:
         line = line.strip()
-        # Split by TAB (key=month, value=temperature)
         parts = line.split('\t')
-        if len(parts) != 2:  # Skip invalid lines
+        if len(parts) != 2:
             continue
         month_str, _ = parts
         try:
             month = int(month_str)
-            unique_months.add(month)  # Add month to set
+            unique_months.add(month)
         except ValueError:
             continue
 
-    # Output total unique months
+
     print(f"Number of months with temperature above threshold: {len(unique_months)}")
 
 if __name__ == "__main__":
