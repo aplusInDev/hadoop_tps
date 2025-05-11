fdadf903ac777fa5f4b1d1e019e9c65175eace82
aplusInDev <laabid.abdessamadplus@gmail.com>
2025-05-11 15:19:31 +0100

    updating apply_1

--- tp2/apply_1.sh
+++ tp2/apply_1.sh
@@ -1,6 +1,13 @@
 hdfs dfs -mkdir -p /data/tp2/input_1
 hdfs dfs -copyFromLocal hadoop_tps/tp2/meteosample.txt /data/tp2/input_1/
 
+cleanup() {
+    echo "Cleaning up HDFS directories..."
+    hdfs dfs -rm -r /data/tp2/ /output_1 2>/dev/null
+}
+
+trap cleanup ERR INT TERM EXIT
+
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
   -files hadoop_tps/tp2/mapper_1.py,hadoop_tps/tp2/reducer_1.py \
   -mapper "python3 mapper_1.py" \
@@ -10,6 +17,3 @@ hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
 
 echo "Results: ------------------";
 hdfs dfs -cat /output_1/part-*
-
-hdfs dfs -rm -r /output_1
-hdfs dfs -rm -r /data/tp2/
