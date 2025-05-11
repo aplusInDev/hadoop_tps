cdd90fcc72a85af013d205310895f4a0a195ee47
aplusInDev <laabid.abdessamadplus@gmail.com>
2025-05-11 15:28:58 +0100

    updating spark settup

--- tp5/setup.sh
+++ tp5/setup.sh
@@ -4,11 +4,11 @@ sudo mv spark-3.5.5-bin-hadoop3 /opt/spark
 cat hadoop_tps/tp5/bashrc >> ~/.bashrc
 source ~/.bashrc
 cp $SPARK_HOME/conf/spark-env.sh.template $SPARK_HOME/conf/spark-env.sh
-cat spark-env.sh >> $SPARK_HOME/conf/spark-env.sh
+cat hadoop_tps/tp5/spark-env.sh >> $SPARK_HOME/conf/spark-env.sh
 cp $SPARK_HOME/conf/workers.template $SPARK_HOME/conf/workers
-cat hadoop_tps/tp5/workers >> $SPARK_HOME/conf/workers
 
 if [ $1 = "master"]; then
+    cat hadoop_tps/tp5/workers >> $SPARK_HOME/conf/workers
     cp $SPARK_HOME/conf/spark-defaults.conf.template $SPARK_HOME/conf/spark-defaults.conf
     cat hadoop_tps/tp5/spark-defaults.conf >> $SPARK_HOME/conf/spark-defaults.conf
 
